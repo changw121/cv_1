ddeac44e870df8ae840b8e939725c2b6a23b75db
changw <2283992824@qq.com>
2022-10-06 09:30:15 +0800

    绘制了八卦图

--- dist/index.de158e3a.js
+++ dist/index.de158e3a.js
@@ -1,10 +1,48 @@
-let demo = document.querySelector("#demo"); //拿到demo
-let string = `
-我是一名前端新人
-接下来我要加样式了
-我要加的样式是
-body{
-    color: red;
+let html = document.querySelector("#html");
+let style = document.querySelector("#style");
+let string = `/* 你好，我叫小常
+ * 接下来我演示一下我的前端功底
+ * 首先我要准备一个div
+ **/
+#div1{
+    border: 1px solid red;
+    width: 200px;
+    height: 200px;
+}
+/* 接下来我把div变成一个八卦图
+ * 首先，把div变成一个圆
+ **/
+#div1 {
+    border-radius: 50%;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    border: none;
+}
+/* 八卦是阴阳形成的
+ * 一黑一白
+ **/
+#div1 {
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+/* 加两个球 */
+#div1::before {
+    width: 100px;
+    height: 100px;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #000;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
+}
+#div1::after{
+    width: 100px;
+    height: 100px;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #fff;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%, rgba(0,0,0,1) 100%);
 }
 `;
 let string2 = ""; //缓存要写到屏幕上的结果
@@ -15,13 +53,18 @@ let step = ()=>{
     setTimeout(()=>{
         if (string[n] === "\n") //如果是回车就不照搬，将string中的回车变成html中的回车
         string2 += "<br>";
+        else if (string[n] === " ") //如果是空格就转化成html中的空格
+        string2 += "&nbsp;";
         else string2 += string[n];
-        demo.innerHTML = string2;
+        html.innerHTML = string2;
+        style.innerHTML = string.substring(0, n);
+        window.scrollTo(0, 99999);
+        html.scrollTo(0, 99999);
         if (n < string.length - 1) {
             n += 1;
             step();
         }
-    }, 100);
+    }, 50);
 };
 step();
 
---
+++ src/reademe.md
@@ -0,0 +1,2 @@
+运行命令
+npx parcel src/index.html 
